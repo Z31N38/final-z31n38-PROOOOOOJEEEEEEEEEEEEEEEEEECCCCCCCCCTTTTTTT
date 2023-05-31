93903dc532dc63135ed5246e31735bf10d036a88
Z31N38 <zeinebsgh7@gmail.com>
2023-05-31 16:03:58 +0100

    project2.0

--- project-interact.js
+++ project-interact.js
@@ -1,20 +1,33 @@
-
-
-const butt=document.getElementsByClassName('grif');
-const navi=document.getElementsByClassName('nav')
-for(let i of butt) {
-    i.addEventListener('click', creat1, function thered() {
-        document.navi.classList.add('red')
-    })
-}
 function griffon(){
     alert("Congratulations you are a Gryffindor!");
-};
-function creat1(){
-var firstd =document.createElement("div");
-var texty=document.createTextNode("this is a something griffy");
-firstd.appendChild(texty);
-var hiss = documet.getElementsByClassName("houses");
-hiss.appendChild(firstd);
+    const body=document.querySelector("body")
+    document.body.style.backgroundColor = 'rgb(95, 14, 14)';
+    const para = document.createElement("p");
+    const node = document.createTextNode("This is new.");
+    para.appendChild(node);
+    
+    const element = document.getElementById("div1");
+    element.innerHTML='';
+    element.appendChild(para);
+    };
 
+
+
+function crow(){
+    alert("Congratulations you are a Ravenclaw!");
+    const body=document.querySelector("body")
+    document.body.style.backgroundColor = 'rgba(12, 12, 131, 0.836)';
 }
+
+function buffoon() {
+     alert("Congratulations you are a Hufflepuff!");
+     const body=document.querySelector("body")
+     document.body.style.backgroundColor = 'rgb(209, 156, 22)';
+    }
+function snake(){
+    alert("Congratulations you are a Slytherin");
+const body=document.querySelector("body")
+    document.body.style.backgroundColor = 'rgb(51, 112, 51)';
+    const nav=document.querySelector("navbar")
+    document.nav.style.backgroundColor = 'rgb(114, 207, 114);';
+}    
